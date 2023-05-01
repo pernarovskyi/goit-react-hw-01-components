2aabb11fdf400b8b9766e5050546a2452cd5d593
ypernarovskyi@softjourn.com <ypernarovskyi@softjourn.com>
2023-05-01 10:04:04 +0300

    Added Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Profile } from "./Profile";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
+      <Profile />
     </div>
   );
 };
